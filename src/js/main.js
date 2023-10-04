93b85653cfc467868e647e21ae0e8c14419d4507
Nitesh Kumar <niteshnunfara0@gmail.com>
2023-10-05 03:47:18 +0530

    doors done

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ function createSimulation() {
     // Clear any previous content
     const levelsContainer = document.getElementById("levelsContainer");
     const liftsContainer = document.getElementById("liftsContainer");
+    const doorsContainer = document.getElementById("doorsContainer");
+
     levelsContainer.innerHTML = "";
     liftsContainer.innerHTML = "";
 
@@ -27,18 +29,28 @@ function createSimulation() {
         const greenButton = document.createElement("button");
         greenButton.textContent = "Green";
         greenButton.className = "button-green";
-        greenButton.onclick = () => moveLiftsUp(level); // Attach click event to the green button
+        greenButton.onclick = () => moveLifts("up", level); // Attach click event to the green button
         console.log(level);
 
-        levelElement.appendChild(greenButton);
+        // levelElement.appendChild(greenButton);
 
         const redButton = document.createElement("button");
         redButton.textContent = "Red";
         redButton.className = "button-red";
-        redButton.onclick = () => moveLiftsDown(level); // Attach click event to the red button
-        levelElement.appendChild(redButton);
+        redButton.onclick = () => moveLifts("down",level); // Attach click event to the red button
+        // levelElement.appendChild(redButton);
 
         levelsContainer.appendChild(levelElement);
+
+        if (level === 1) {
+            levelElement.appendChild(redButton); // At the top, add only the red button
+        } else if (level === numLevels) {
+            levelElement.appendChild(greenButton); // At the bottom, add only the green button
+        } else {
+            // For levels in between, add both green and red buttons
+            levelElement.appendChild(greenButton);
+            levelElement.appendChild(redButton);
+        }
     }
 
     // Create and display the lifts
@@ -50,92 +62,94 @@ function createSimulation() {
         liftsContainer.appendChild(liftElement);
         console.log(liftElement);
     }
+
+    for (let lift = 1; lift <= numLifts; lift++) {
+        const liftElement = document.createElement("div");
+        liftElement.className = "door";
+        liftElement.textContent = `Gift ${lift}`;
+        doorsContainer.appendChild(liftElement);
+        console.log(liftElement);
+    }
 }
 
 const maxHeight = 5;
+const minHeight = 0; 
 
 // function moveLiftsUp(level) {
+//     // Calculate the difference between the clicked level and the current level
+//     const levelDifference = level - count;
 
-//     val = Math.abs(level - count);
-//     count = level;
-//     console.log(val, "->", level);
+//     // Check if the lift is within the allowed range
+//     if (count + levelDifference <= maxHeight) {
+//         // Update count with the current level
+//         count += levelDifference;
 
-//     console.log(count + val);
-
-//     // Move all lifts up (you can adjust the distance they move)
-//     for (let i = 0; i < liftPositions.length; i++) {
-//         liftPositions[i] -= val; // Adjust the position (e.g., move up by 1)
-//         document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
+//         // Move all lifts up by the calculated level difference
+//         for (let i = 0; i < liftPositions.length; i++) {
+//             liftPositions[i] -= levelDifference; // Adjust the position
+//             document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
+//         }
+//     } else {
+//         console.log('Lift cannot go higher than the maximum allowed height.');
 //     }
 // }
 
 // function moveLiftsDown(level) {
-//     moveLiftsToLevel(level);
+//     // Calculate the difference between the clicked level and the current level
+//     const levelDifference = count - level;
 
-//     // Move all lifts down (you can adjust the distance they move)
-//     for (let i = 0; i < liftPositions.length; i++) {
-//         liftPositions[i] += 1; // Adjust the position (e.g., move down by 1)
-//         document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
+//     // Check if the lift is within the allowed range
+//     if (count - levelDifference >= minHeight) {
+//         // Update count with the current level
+//         count -= levelDifference;
+
+//         // Move all lifts down by the calculated level difference
+//         for (let i = 0; i < liftPositions.length; i++) {
+//             liftPositions[i] += levelDifference; // Adjust the position
+//             document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
+//         }
+//     } else {
+//         console.log('Lift cannot go lower than the minimum allowed height.');
 //     }
 // }
 
-const minHeight = 0; // Set the minimum allowed height
-
-function moveLiftsUp(level) {
+function moveLifts(direction, level) {
     // Calculate the difference between the clicked level and the current level
-    const levelDifference = level - count;
+    const levelDifference = direction === 'up' ? level - count : count - level;
+    const animationDuration = `${Math.abs(levelDifference) * 2}s`;
 
     // Check if the lift is within the allowed range
-    if (count + levelDifference <= maxHeight) {
-        // Update count with the current level
-        count += levelDifference;
-
+    if (direction === 'up' && count + levelDifference <= maxHeight) {
         // Move all lifts up by the calculated level difference
         for (let i = 0; i < liftPositions.length; i++) {
             liftPositions[i] -= levelDifference; // Adjust the position
-            document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
         }
-    } else {
-        console.log('Lift cannot go higher than the maximum allowed height.');
-    }
-}
-
-function moveLiftsDown(level) {
-    // Calculate the difference between the clicked level and the current level
-    const levelDifference = count - level;
-
-    // Check if the lift is within the allowed range
-    if (count - levelDifference >= minHeight) {
-        // Update count with the current level
-        count -= levelDifference;
-
+        count += levelDifference;
+    } else if (direction === 'down' && count - levelDifference >= minHeight) {
         // Move all lifts down by the calculated level difference
         for (let i = 0; i < liftPositions.length; i++) {
             liftPositions[i] += levelDifference; // Adjust the position
-            document.querySelectorAll('.lift')[i].style.transform = `translateY(${liftPositions[i] * 97}px)`; // Adjust the lift position in pixels
         }
+        count -= levelDifference;
     } else {
-        console.log('Lift cannot go lower than the minimum allowed height.');
+        console.log('Lift cannot go beyond the allowed height range.');
+    }
+
+    // Update the lift positions and their doors in the DOM
+    const liftElements = document.querySelectorAll('.lift');
+    const doorElements = document.querySelectorAll('.door');
+    
+    for (let i = 0; i < liftPositions.length; i++) {
+        const newPosition = liftPositions[i] * 147.5;
+        liftElements[0].style.top = `${newPosition}px`; // Adjust the lift position in pixels
+        doorElements[0].style.top = `${newPosition}px`; // Adjust the door position in pixels0
+        liftElements[0].style.transitionDuration = animationDuration;
+        doorElements[0].style.transitionDuration = animationDuration; // Set the animation duration
     }
 }
 
 
 
 
-function moveLiftsToLevel(targetLevel, count) {
-    // Update the current level for each lift
-    for (let i = 0; i < liftLevels.length; i++) {
-        liftLevels[i] = targetLevel;
-    }
 
-    let currLevel = Math.abs(count - targetLevel);
-    count = currLevel;
-    console.log(targetLevel, currLevel)
-    console.log(count);
-
-    // Display the level number as output
-    const outputContainer = document.getElementById("outputContainer");
-    outputContainer.textContent = `Lifts are now at Level ${targetLevel}`;
-
-}
 
--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 // Store the current level of each lift (initialized to 1)
 const liftLevels = [];
+const maxHeight = 5; // Adjust the maximum height as needed
+const minHeight = 1; // Adjust the minimum height as needed
 
 function createSimulation() {
     const numLevels = parseInt(document.getElementById("numLevels").value);
@@ -11,32 +13,33 @@ function createSimulation() {
     levelsContainer.innerHTML = "";
     liftsContainer.innerHTML = "";
 
-    // Initialize lift levels and positions
-    for (let lift = 1; lift <= numLifts; lift++) {
-        liftLevels.push(1); // Initialize lift levels to 1
-        // testing
-    }
-
     // Create and display the levels
-    for (let level = 1; level <= numLevels; level++) {
+    for (let level = numLevels; level >= 1; level--) {
         const levelElement = document.createElement("div");
         levelElement.className = "level";
-        levelElement.textContent = `Level ${level}`;
 
         // Create green and red buttons for each level
         const greenButton = document.createElement("button");
         greenButton.textContent = "Green";
         greenButton.className = "button-green";
-        greenButton.onclick = () => moveLiftsToLevel(level); // Attach click event to the green button
-        levelElement.appendChild(greenButton);
+        greenButton.onclick = () => moveLiftToLevel(level); // Attach click event to the green button
 
         const redButton = document.createElement("button");
         redButton.textContent = "Red";
         redButton.className = "button-red";
-        redButton.onclick = () => moveLiftsToLevel(level); // Attach click event to the red button
-        levelElement.appendChild(redButton);
+        redButton.onclick = () => moveLiftToLevel(level); // Attach click event to the red button
 
         levelsContainer.appendChild(levelElement);
+
+        if (level === 1) {
+            levelElement.appendChild(redButton); // At the top, add only the red button
+        } else if (level === numLevels) {
+            levelElement.appendChild(greenButton); // At the bottom, add only the green button
+        } else {
+            // For levels in between, add both green and red buttons
+            levelElement.appendChild(greenButton);
+            levelElement.appendChild(redButton);
+        }
     }
 
     // Create and display the lifts
@@ -44,24 +47,25 @@ function createSimulation() {
         const liftElement = document.createElement("div");
         liftElement.className = "lift";
         liftElement.textContent = `Lift ${lift}`;
+        liftLevels.push(1); // Initialize lift levels to 1
         liftsContainer.appendChild(liftElement);
     }
 }
 
-function moveLiftsToLevel(targetLevel) {
-    // Update the current level for each lift
-    for (let i = 0; i < liftLevels.length; i++) {
-        liftLevels[i] = targetLevel;
-    }
+function moveLiftToLevel(targetLevel) {
+    // Check if the target level is within the allowed range
+    if (targetLevel >= minHeight && targetLevel <= maxHeight) {
+        // Update the current level for each lift
+        for (let i = 0; i < liftLevels.length; i++) {
+            liftLevels[i] = targetLevel;
+            const liftElement = document.querySelectorAll('.lift')[i];
+            liftElement.style.top = `${targetLevel * 97}px`; // Adjust the lift position based on levels
+        }
 
-    // Display the level number as output
-    const outputContainer = document.getElementById("outputContainer");
-    outputContainer.textContent = `Lifts are now at Level ${targetLevel}`;
-
-    // Move the lifts to their respective levels
-    for (let i = 0; i < liftLevels.length; i++) {
-        const liftElement = document.querySelectorAll('.lift')[i];
-        const targetPosition = (targetLevel - 1) * 30; // Adjust the lift position in pixels
-        liftElement.style.transform = `translateY(${targetPosition}px)`;
+        // Display the level number as output
+        const outputContainer = document.getElementById("outputContainer");
+        outputContainer.textContent = `Lifts are now at Level ${targetLevel}`;
+    } else {
+        console.log('Invalid level. Lift cannot go higher than the maximum allowed height or lower than the minimum allowed height.');
     }
 }
